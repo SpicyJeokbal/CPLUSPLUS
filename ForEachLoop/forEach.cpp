77c17f060e16e159f7023e78cbf9edd882408409
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-06-24 14:11:18 +0800

    For Each Loop

---
+++ ForEachLoop/forEach.cpp
@@ -0,0 +1,18 @@
+/* 
+#5
+Name: Arvin Liel Obayan
+Date: 06/24/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int Numbers[5] = {1,2,3,4,5};
+    for (int i : Numbers){
+        cout << i << " ";
+    }
+
+    return 0;
+}
