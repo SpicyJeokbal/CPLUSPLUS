a44edca705874d2de1324ff4aad340f1821ec606
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-06-24 11:38:44 +0800

    Ternary operation

---
+++ Ternary Operator/Ternary.cpp
@@ -0,0 +1,21 @@
+/* 
+#3
+Name: Arvin Liel Obayan
+Date: 06/24/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    /* variable = (condition) ? expressionTrue : expressionFalse */
+
+    int x = 10;
+    bool y = (x > 11) ? true : false;
+    cout << y;
+
+
+    return 0;
+}
