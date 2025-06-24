1fc9b44e7c23a66f4f22b2e258eb2d4dafa763e2
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-06-24 10:05:36 +0800

    Update Strings.cpp with latest changes

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     
     string greeting = "hello ", name = "Cailegh", surname = " Winters", fullname = name.append(surname);
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string sample = "We are the so-called \"Vikings\" from the north.";
+    string Fullname;
+
 
     cout << greeting + name << endl;
-    cout << fullname;
+    cout << fullname << endl;
+    cout << txt.length() << endl;
+    cout << txt.size() << endl;
+    cout << txt[txt.length() - 1] << endl;
+    cout << sample << endl;
+
+    cout << "Enter your fullname: ";
+    cin >> Fullname; // cin considers white space as a terminating character, which means it can only store a single word
+
+    cout << "Enter your fullname: ";
+    getline(cin, fullname); 
+    /*That's why, when working with strings, we often use the getline()
+    function to read a line of text. It takes cin as the first parameter, and the string variable as second:*/
 
 
     return 0;
