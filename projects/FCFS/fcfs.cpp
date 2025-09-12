1171147e0ab61138110bfc85b808360c9d91c656
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-09-12 20:28:40 +0800

    first come first serve

---
+++ projects/FCFS/fcfs.cpp
@@ -0,0 +1,64 @@
+
+
+#include <iostream>
+
+struct Process{
+    int id;
+    int arrivalTime;
+    int burstTime;
+    int waiting;
+    int turnaround;
+};
+
+
+
+int main(){
+    
+    Process p[20];
+
+    int n{};
+    std::cout << "Number of processes: ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++){
+        p[i].id = i+1;
+        std::cout << p[i].id << " arrival time: ";
+        std::cin >> p[i].arrivalTime;
+        std::cout << p[i].burstTime << " burst time: ";
+        std::cin >> p[i].burstTime;
+    }
+
+    for(int i=0; i < n-1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(p[i].arrivalTime > p[j].arrivalTime){
+                std::swap(p[i], p[j]);
+            }
+        }
+    }
+
+    int currentTime = 0;
+    for(int i = 0; i < n; i++){
+        if(currentTime < p[i].arrivalTime){
+            currentTime = p[i].arrivalTime;
+        }
+
+        p[i].waiting = currentTime - p[i].arrivalTime;
+        currentTime += p[i].burstTime;
+        p[i].turnaround = p[i].waiting + p[i].burstTime;
+    }
+
+    std::cout << "\nID\tArrival\tBurst\tWaiting\tTurnaround\n";
+    for(int i=0; i<n; i++) {
+        std::cout << p[i].id << "\t"
+             << p[i].arrivalTime << "\t"
+             << p[i].burstTime << "\t"
+             << p[i].waiting << "\t"
+             << p[i].turnaround << "\n";
+    }
+
+
+    
+    
+
+    return 0;
+}  
