22f1a557d9f0e6ccc64a14540bae349edd22e33c
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-10-21 15:57:05 +0800

    Updated Parallel HTTP Client

--- projects/HTTP SERVER/Parallel HTTP Server/CLIENT/receive.cpp
+++ projects/HTTP SERVER/Parallel HTTP Server/CLIENT/receive.cpp
@@ -6,13 +6,48 @@
 #pragma comment(lib, "ws2_32.lib")
 
 
+bool initializedWinsock();
+SOCKET createClientSocket();
+void handleServer(SOCKET& clientSocket);
+
+
 //CLIENT SIDE
 int main(){
     
-    WSAData wsa;
-    WSAStartup(MAKEWORD(2,2), &wsa);
+    if (!initializedWinsock()) return 1;
 
+    SOCKET clientSocket = createClientSocket();
+    if(clientSocket == INVALID_SOCKET) return 1;
+
+    handleServer(clientSocket);
+    
+    
+    return 0;
+
+}
+
+bool initializedWinsock(){
+    WSAData wsa;
+    int result = WSAStartup(MAKEWORD(2,2), &wsa);
+
+    if (result != 0){
+        std::cerr << "WSAStartup failed! Error code: " << result << "\n";
+        return false;
+    }
+
+    std::cout << "Winsock initialized\n";
+    std::cout << "Description " << wsa.szDescription << "\n";
+    return true;
+
+}
+
+SOCKET createClientSocket(){
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(clientSocket == INVALID_SOCKET){
+        std::cerr << "Client socket creation failed. Error: " << WSAGetLastError << "\n";
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
 
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
@@ -24,16 +59,19 @@ int main(){
         std::cerr << "Connection failed. Error: " << WSAGetLastError() << "\n";
         closesocket(clientSocket);
         WSACleanup();
-        return 1;
+        return INVALID_SOCKET;
     }
 
     std::cout << "Client to Server connection established\n";
+    return clientSocket;
+}
 
+void handleServer(SOCKET& clientSocket){
     char buffer[4096];
     int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
     if(bytesReceived > 0){
         buffer[bytesReceived] = '\0'; //null terminate 
-        std::cout << "Server says " << buffer << "\n";
+        std::cout << "Request:\n" << buffer << "\n";
         
         const char *clientReply = "Message received\n";
         send(clientSocket, clientReply, strlen(clientReply), 0);
@@ -41,7 +79,4 @@ int main(){
 
     closesocket(clientSocket);
     WSACleanup();
-    
-    return 0;
-
 }
